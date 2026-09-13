0c0d8787f95aa27ca78f81970e9bde4730fa40a2
Abdoul-Gafar AMADOU <abdoulgafaramadou938@gmail.com>
2026-09-13 23:36:45 +0000

    feat: modeliser Salle et Reservation

---
+++ src/main/java/sn/woy/domain/Reservation.java
@@ -0,0 +1,38 @@
+package sn.woy.domain;
+
+import java.time.LocalDateTime;
+
+public final class Reservation extends AbstractEntity {
+    public Reservation() {
+        super();
+    }
+
+    private LocalDateTime dateDebut;
+    private LocalDateTime dateFin;
+
+    private Salle salle;
+
+    public Salle getSalle() {
+        return salle;
+    }
+
+    public void setSalle(Salle salle) {
+        this.salle = salle;
+    }
+
+    public LocalDateTime getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDateTime dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public LocalDateTime getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDateTime dateFin) {
+        this.dateFin = dateFin;
+    }
+}
---
+++ src/main/java/sn/woy/domain/Salle.java
@@ -0,0 +1,38 @@
+package sn.woy.domain;
+
+import java.util.List;
+
+public final class Salle extends AbstractEntity {
+    public Salle() {
+        super();
+    }
+
+    private List<Reservation> reservations;
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    private String code;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
